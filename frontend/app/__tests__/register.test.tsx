40bfe3bc741e303fd257fbb6c6ee338f08a926ce
Pablo Heredia <pheredia2004@gmail.com>
2024-11-27 19:31:29 -0600

    More tests

--- frontend/app/__tests__/register.test.tsx
+++ frontend/app/__tests__/register.test.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { render, fireEvent, waitFor } from "@testing-library/react-native";
 import RegisterForm from "../register"; // Ruta según tu estructura de carpetas.
+import { Alert } from "react-native";
 
 jest.mock("expo-router", () => ({
   useRouter: () => ({ back: jest.fn(), replace: jest.fn() }),
 }));
 
-
+afterEach(() => {
+    jest.restoreAllMocks(); // Limpia todos los mocks después de cada prueba.
+  });
 
 describe("RegisterForm", () => {
   it("renders all input fields correctly", () => {
@@ -28,5 +31,47 @@ describe("RegisterForm", () => {
     expect(nameInput.props.value).toBe("Juan Pérez");
   });
 
+  it("shows an alert when required fields are missing", async () => {
+    jest.spyOn(Alert, "alert"); // Espiar el método Alert.alert.
+  
+    const { getByText } = render(<RegisterForm />);
+  
+    const registerButton = getByText("Registrarse");
+    fireEvent.press(registerButton); // Simula el clic en el botón.
+  
+    await waitFor(() => {
+      expect(Alert.alert).toHaveBeenCalledWith(
+        "Error",
+        "Por favor, rellena todos los campos obligatorios."
+      );
+    });
+
+  });
+
+  it("shows an alert when the password is too short", async () => {
+    jest.spyOn(Alert, "alert");
+  
+    const { getByPlaceholderText, getByText } = render(<RegisterForm />);
+  
+    fireEvent.changeText(getByPlaceholderText("Ingresa tu nombre"), "Juan Pérez");
+    fireEvent.changeText(getByPlaceholderText("nombre@ejemplo.com"), "juan.perez@example.com");
+    fireEvent.changeText(getByPlaceholderText("(331) 538-4179"), "3315384179");
+    fireEvent.changeText(getByPlaceholderText("************"), "123"); // Contraseña corta.
+  
+    const registerButton = getByText("Registrarse");
+    fireEvent.press(registerButton);
+  
+    await waitFor(() => {
+      expect(Alert.alert).toHaveBeenCalledWith(
+        "Error",
+        "La contraseña debe tener al menos 6 caracteres."
+      );
+    });
+  
+    jest.restoreAllMocks();
+  });
+  
+
+  
  
 });
